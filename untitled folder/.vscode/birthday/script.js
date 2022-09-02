910709bdb38e10c2e550f8d66108eb9017496c0e
Leen AlZoughbi <leenalzoughbi2007@gmail.com>
2022-09-02 18:51:51 +0300

    changing title

---
+++ untitled folder/.vscode/birthday/script.js
@@ -0,0 +1,12 @@
+const first_page= document.getElementById("first_page")
+
+
+function open_card(){
+    document.getElementById("first_page").className="gift"
+
+}
+
+function close_card(){
+  document.getElementById("first_page").className=""
+  console.log("heyy")
+}
